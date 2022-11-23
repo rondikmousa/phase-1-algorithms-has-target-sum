9e446cbfc12bd01c4098f9dcc72ff77810e0734e
rondikmousa <rondikmousaa@gmail.com>
2022-11-24 00:50:38 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const seenNumbers = {};
+
+  for (const number of array) {
+    // n steps
+    const complement = target - number;
+    if (complement in seenNumbers) return true;
+    seenNumbers[number] = true;
+  }
+
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
 */
+function findSock(array) {
+  for (const item of array) {
+    if (item === "sock") return "sock";
+  }
+}
 
+// O(1) runtime
+function findSock(object) {
+  if (object.sock) return "sock";
+}
 /* 
   Add your pseudocode here
 */
-
+hasTargetSum([1,2,3,4], 6)
+  seenNumbers = {
+    1: true,
+    2: true,
+    3: true
+  }
+ 
 /*
-  Add written explanation of your solution here
+  create an object to keep track of numbers we've already seen
+  iterate through each number in the array
+    for the current num, identify a complement that adds to the target (comp = target - num)
+    check if any key on our object is the complement
+      if so, return true
+      otherwise, add that number to the object
+  if I reach the end of the array, return false 
+Add written explanation of your solution here
 */
 
 // You can run `node index.js` to view these console logs
